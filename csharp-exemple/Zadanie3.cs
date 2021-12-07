346dea425408a42dde62c2d3ee4145fa7db7e14a
Maksim Nizovtsev <senslider@gmail.com>
2021-12-07 21:21:00 +0100

    Задание 3: позитивный сценарий логина

---
+++ csharp-exemple/Zadanie3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class Zadanie3
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private readonly By LoginInputButtom = By.XPath("//input[@type='text']");
+        private const string LoginInput = "admin";
+        private readonly By PasswordInputButtom = By.XPath("//input[@type='password']");
+        private const string PasswordInput = "admin";
+        private readonly By LoginButtom = By.XPath("//button[@type='submit']");
+        
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Login()
+        {
+            driver.Url = "http://localhost/litecart/admin";
+            driver.Manage().Window.Maximize();
+            
+            var login = driver.FindElement(LoginInputButtom);
+            login.SendKeys(LoginInput);
+            
+            var password = driver.FindElement(PasswordInputButtom);
+            password.SendKeys(PasswordInput);
+
+            var enter = driver.FindElement(LoginButtom);
+            enter.Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+           driver.Quit();
+           driver = null;
+        }
+    }
+}
