bb6e8c25334c1474c7b998bb5865653277f91623
Maksim Nizovtsev <senslider@gmail.com>
2022-01-04 12:26:28 +0100

    Задание 8. Проверить сортировку стран и геозон на странице стран

---
+++ csharp-exemple/Zadanie8.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class Zadanie8
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+            driver.FindElement(By.XPath("//input[@type='text']")).SendKeys("admin");
+            driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("admin");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+        }
+
+        [Test]
+        public void CountryZoneSort()
+        {
+            var countries = driver.FindElements(By.XPath("//tr[@class='row']/td[5]/a"));
+            var countryNames = new List<string>();
+
+            foreach (IWebElement country in countries)
+            {
+                countryNames.Add(country.Text);
+            }
+
+            var countryNamesSorted = new List<string>(countryNames);
+            countryNamesSorted.Sort();
+
+            CollectionAssert.AreEqual(countryNamesSorted, countryNames);
+
+            for (int i = 1; i <= countries.Count; i++)
+            {
+                if (!driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td[6]")).Text.Equals("0"))
+                {
+                    driver.FindElement(By.XPath("//tr[@class='row'][" + i + "]/td[5]/a")).Click();
+                    var geoZones = driver.FindElements(By.CssSelector("table.dataTable td:nth-of-type(3) input[type=hidden]"));
+                    var geoZonesNames = new List<string>();
+
+                    foreach (IWebElement geoZone in geoZones)
+                    {
+                        geoZonesNames.Add(geoZone.Text);
+                    }
+
+                    var geoZonesNamesSorted = new List<string>(geoZonesNames);
+                    geoZonesNamesSorted.Sort();
+
+                    CollectionAssert.AreEqual(geoZonesNamesSorted, geoZonesNames);
+
+                    driver.Navigate().Back();
+                }
+            }
+        }
+
+        [TearDown]
+        public void stop()
+        {
+          driver.Quit();
+          driver = null;
+        }
+    }
+}
