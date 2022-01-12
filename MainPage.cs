7c7987a9060223c4357580a9fd1d5cf85e53a45b
SenslideR <65404709+SenslideR@users.noreply.github.com>
2022-01-12 18:37:29 +0100

    Delete MainPage.cs

--- MainPage.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium;
-
-namespace SeleniumHomework.PageObject
-{
-    public class MainPage : BasePage
-    {
-        public By FirstProduct => By.CssSelector("div.image-wrapper:nth-of-type(1)");
-    }
-}
