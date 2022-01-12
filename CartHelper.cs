12e3324d38d00f736bab93e4039adf97625d541b
Maksim Nizovtsev <senslider@gmail.com>
2022-01-12 19:19:17 +0100

    19

---
+++ CartHelper.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumHomework.PageObject
+{
+    public class CartHelper : HelperBase
+    {
+        public CartHelper(IWebDriver driver) : base(driver) { }
+
+        public void EmptyCart()
+        {
+            var shortcuts = driver.FindElements(cartPage.Table);
+
+            foreach (var shortcut in shortcuts)
+            {
+                var table = driver.FindElement(cartPage.Table);
+                wait.Until(driver => driver.FindElement(cartPage.RemoveButton).Displayed);
+                driver.FindElement(cartPage.RemoveButton).Click();
+                wait.Until(ExpectedConditions.StalenessOf(table));
+            }
+        }
+    }
+}
